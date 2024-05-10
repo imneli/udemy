f0a822c7831f3b89c3f37f26daad76810403ae6f
imneli <contatodoneli@gmail.com>
2024-05-09 23:33:04 -0300

    aula 13: strings

---
+++ S2: JS BASIC/aula13.js
@@ -0,0 +1,7 @@
+let umaString = "um texto";
+
+console.log(umaString.charAt(4));
+console.log(umaString.lastIndexOf('o', 3));
+console.log(umaString.match(/[a-z]/));
+console.log(umaString.replace('Um', 'Outra'));
+console.log(umaString.length)
