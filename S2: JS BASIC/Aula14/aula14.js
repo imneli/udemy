64d9370e8cf678eef634c37e00f8577fa9001b4d
imneli <contatodoneli@gmail.com>
2024-05-11 14:47:59 -0300

    ex number & math

---
+++ S2: JS BASIC/Aula14/aula14.js
@@ -0,0 +1,22 @@
+// numbers
+// IEEE 754-2008
+
+
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 += num2 // num1 = num1 + num2;
+
+// console.log(num1.toString() + num2);
+// num1 = num.toString();
+// console.log(num1.toString(2));
+// console.log(num1.toFixed(2));
+
+
+// let temp = num1 * '5'
+// console.log(Number.isNaN(temp))
+
+console.log(num1.toString() + num2);
+console.log(typeof num1);
+console.log(Number.isInteger(num1));
+
---
+++ S2: JS BASIC/Aula15/aula15.js
@@ -0,0 +1,8 @@
+let num1 = 9.2134;
+//let num2 = Math.floor(num1);
+// let num2 = Math.ceil(num1);
+// let num2 = Math.round(num1);
+console.log(Math.max(1,2,3,4,5,-10,-20,-50,1500,9,8,7));
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+console.log(aleatorio);
+console.log(num2)
