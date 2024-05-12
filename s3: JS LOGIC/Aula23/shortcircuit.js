87f0cd092fae6df868989a86e9478ea26905495c
imneli <contatodoneli@gmail.com>
2024-05-11 22:27:31 -0300

    Aula 23: True or False

---
+++ s3: JS LOGIC/Aula23/shortcircuit.js
@@ -0,0 +1,27 @@
+/*
+&& -> false && true -> false (o valor)
+|| -> false || true -> true ()
+
+FALSY
+false
+0
+'' "" ``
+null / undefined
+NaN
+*/
+const corUsuario = null; // vermelho / console = vermelho ! preto
+const corPadrao = corUsuario || 'Preto';
+
+// console.log('Luiz otavio' && true && NaN);
+// console.log('Luiz' && 'Maria');
+
+function falaOi () {
+    return 'Oi';
+}
+
+const vaiExecutar = true;
+if (vaiExecutar === true) {
+    console.log(vaiExecutar && falaOi());
+} else {
+    console.log('ERRO! FALSE');
+}
