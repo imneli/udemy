31289b78d2ecc9113e3d57880a864d8e6e0e4912
imneli <contatodoneli@gmail.com>
2024-05-13 17:22:15 -0300

    Aula 25: If & Else (2)

---
+++ s3: JS LOGIC/Aula25/if_else2.js
@@ -0,0 +1,11 @@
+const num = 7
+
+if (num >= 0 && num <= 5)  {
+    console.log('the number is between 0 and 5');
+} else if (num >= 6 && num <= 8) {
+    console.log('the number is between 6 and 8');
+} else if (num >= 9 && num <= 11) {
+    console.log('the number is between 9 and 11');
+} else {
+    console.log('the number is not between 0 and 11')
+}
