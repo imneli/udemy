cbc544121e4abd59c4be385ce639f8acabc56655
imneli <contatodoneli@gmail.com>
2024-05-15 17:07:22 -0300

    Aula 34: For Classic

---
+++ s3: JS LOGIC/Aula34/for.js
@@ -0,0 +1,24 @@
+// console.log('line0');
+// console.log('line1');
+// console.log('line2');
+// console.log('line3');
+// console.log('line4');
+// console.log('line5');
+
+// i - index
+
+
+//       i        cont   order (add 1 a cada laço)
+for (let i = 0; i <= 10; i++) { // de 10 em 10 (i+= 10)
+    console.log(`line ${i}`);
+} 
+
+
+
+const fruits = ['banana', 'manga', 'uva'];
+
+console.log(fruits.length) // len array
+
+ for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+ }
