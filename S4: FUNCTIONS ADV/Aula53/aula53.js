d9618261ac63f21825bebd1f3f2e14f768221e34
imneli <contatodoneli@gmail.com>
2024-05-20 17:47:48 -0300

    Aula 53: Closures

---
+++ S4: FUNCTIONS ADV/Aula53/aula53.js
@@ -0,0 +1,14 @@
+function retornaFuncao(nome) {
+    return function() {
+        return nome;
+  };
+}
+
+const funcao = retornaFuncao('luiz');
+const funcao2 = retornaFuncao('joao');
+console.dir(funcao());
+console.dir(funcao2());
+
+console.log(funcao(), funcao2())
+
+// closure é a habilidade de acessar o escopo lexico
