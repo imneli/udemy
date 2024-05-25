c83fd573c68c54b7fdef90cc3c869c916eea892f
imneli <contatodoneli@gmail.com>
2024-05-25 16:20:00 -0300

    Aula 64: Concatenação de Arrays

---
+++ S5: ARRAYS ADV/Aula64/aula64.js
@@ -0,0 +1,9 @@
+const a1 = [1,2,3];
+const a2 = [4,5,6];
+// const a3 = a1.concat(a2, [7,8,9], 'matheus');
+
+// ... rest -> spread
+
+const a3 = [...a1, 'Matheus', ...a2, ...[7,8,9]];
+console.log(a3)
+
