470c8041f6dc367982fbaf1823c0bb3986482b0a
imneli <contatodoneli@gmail.com>
2024-06-01 00:04:45 -0400

    Aula 76: Herança

---
+++ S6_ OBJ & PROTO/Aula76/aula76.js
@@ -0,0 +1,64 @@
+// t-shirt, mug
+
+function Product(name, price) {
+   this.name = name;
+   this.price = price; 
+}
+
+const p1 = new Product('t-shirt', 50)
+
+Product.prototype.aumento = function(valor) {
+    this.price += valor;
+}
+
+Product.prototype.desconto = function(valor) {
+    this.price -= valor;
+}
+
+function Camiseta(name, price, color) {
+    Product.call(this, name, price);
+    this.color = color;
+}
+
+Camiseta.prototype = Object.create(Product.prototype);
+Camiseta.prototype.constructor = Camiseta;
+
+Camiseta.prototype.aumento = function(percentual) {
+    this.price = this.price + (this.price * (percentual / 100))
+}
+
+function Mug(name, price, material, stock) {
+    Product.call(this, name, price)
+    this.material = material;
+    this.stock = stock;
+
+    Object.defineProperty(this, 'stock', {
+        enumerable: true,
+        configurable: false,
+        get: function() {
+            return stock;
+        },
+        set: function(valor) {
+            if (typeof valor !== 'number') return;
+            stock = valor;
+        }
+    });
+}
+
+Mug.prototype = Object.create(Product.prototype);
+Mug.prototype.constructor = Mug;
+
+
+
+const produto = new Product('gen', 113)
+const camiseta = new Camiseta('Regata', 30, 'black');
+const mug = new Mug('Mug', 30, 'porcelain', 10);
+mug.stock = 10
+
+
+camiseta.aumento(5);
+
+console.log(mug)
+console.log(mug.stock)
+console.log(produto);
+console.log(camiseta);
