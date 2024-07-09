21c605f0c9b03b5ad65e5334bc598ed9fb008146
imneli <contatodoneli@gmail.com>
2024-07-09 15:56:43 -0300

    Aula 6: CPF e IPS

--- EXP_REG-REGEX/base.js
+++ EXP_REG-REGEX/base.js
@@ -20,10 +20,46 @@ const html = '<p>ola mundo</p> <p>ola dnv</p> <div>sou uma div</div>'
 
 const alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 0123456789'
 
+const cpfs = `
+Os CPFs são:
+  254.224.877-45 215.978.456-12 047.258.369-96 
+
+
+  963.987.321-00
+
+  963.987.32a.00 (NÃO VÁLIDO)
+  963.987.32-00 (NÃO VÁLIDO)
+`;
+
+const cpfs2 = `254.224.877-45
+215.978.456-12
+047.258.369-96
+963.987.32a.00`;
+
+const ips = `
+
+Os Ips são:
+   0.0.0.0
+
+   192.168.0.25
+
+        10.10.5.12
+
+        10.01.10.20 (ESTE IP NÃO É VÁLIDO)
+        10.021.08.20 (ESTE IP NÃO É VÁLIDO)
+
+   255.255.255.255
+
+`;
+
 module.exports = {
     texto,
     texto2,
     arquivos,
     html,
-    alfabeto
+    alfabeto,
+    cpfs,
+    ips,
+    cpfs2
 }
+
